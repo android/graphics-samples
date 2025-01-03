8a89603d3041fe44c19e6b1fdc454a4657234865
SevenJustin <sevenjustin@163.com>
2025-01-03 17:54:39 +0800

    update
    update with local test

--- PdfRendererBasic/Application/src/main/java/com/example/android/pdfrendererbasic/MainActivity.kt
+++ PdfRendererBasic/Application/src/main/java/com/example/android/pdfrendererbasic/MainActivity.kt
@@ -16,52 +16,13 @@
 
 package com.example.android.pdfrendererbasic
 
-import android.app.Dialog
-import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.commitNow
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity(R.layout.main_activity) {
-
-    companion object {
-        const val FRAGMENT_INFO = "info"
-    }
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (savedInstanceState == null) {
-            supportFragmentManager.commitNow {
-                replace(R.id.container, PdfRendererBasicFragment())
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_info -> {
-                InfoFragment().show(supportFragmentManager, FRAGMENT_INFO)
-                return true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    class InfoFragment : DialogFragment() {
-        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-            return AlertDialog.Builder(requireContext())
-                .setMessage(R.string.intro_message)
-                .setPositiveButton(android.R.string.ok, null)
-                .show()
-        }
-    }
-
 }
